a07bfd04efbdd91210f09f575d1e703b015c7baa
komerela <kmwongera@icloud.com>
2019-12-20 20:32:08 -0800

    prod module added to be pushed for gcp settings

---
+++ portfolio_bs/portfolio_bs/prod.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'dotkoome',
+        'USER': 'dotkoome',
+        'PASSWORD': 'Mwongera81?',
+        'HOST': '127.0.0.1',
+        'PORT': '5000',
+    }
+}
