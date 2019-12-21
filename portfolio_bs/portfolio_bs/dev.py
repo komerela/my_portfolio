5c018ba00c5d63658b69111441672bf9e2640811
komerela <kmwongera@icloud.com>
2019-12-20 22:29:13 -0800

    added dev and prod Ip addresses

--- portfolio_bs/portfolio_bs/dev.py
+++ portfolio_bs/portfolio_bs/dev.py
@@ -3,8 +3,8 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'portfolio',
         'USER': 'dotkoome',
-        'PASSWORD': 'Mwongera81?',
+        'PASSWORD': 'Mw0ngera81?',
         'HOST': '127.0.0.1',
-        'PORT': '5000',
+        'PORT': '5432',
     }
 }
