718d178770443a241065333d10fa3325b07b3b49
root <root@portfolio-instance.us-west2-a.c.perimapsite.internal>
2019-12-26 03:52:18 +0000

    Changed path for settings .py by removing dir port

--- portfolio_bs/portfolio_bs/settings.py
+++ portfolio_bs/portfolio_bs/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'portfolio_bs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/koome/venv/portfolio/portfolio_bs/portfolio_bs/templates/port/'],
+        'DIRS': ['/home/koome/venv/portfolio/portfolio_bs/portfolio_bs/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
