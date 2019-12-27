7a38002de587a689869bc618124fc99b278e845a
DenoGeek <kariukidennisde@gmail.com>
2019-12-27 13:14:35 +0300

    Update the static dirs

--- portfolio_bs/portfolio_bs/settings.py
+++ portfolio_bs/portfolio_bs/settings.py
@@ -58,6 +58,7 @@ ROOT_URLCONF = 'portfolio_bs.urls'
     see how i implemented
     'DIRS': [BASE_DIR + '/portfolio_bs/templates/',],
 '''
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -124,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
